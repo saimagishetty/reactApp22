18b36245557d28f29359f718a0bae02e445ff9cc
Magishetty Saikumar <saimagishetty01@gmail.com>
2023-09-22 21:56:42 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState();
+  const x = new XMLHttpRequest()
+  const getdata = () =>{
+    x.open('GET', 'https://api.publicapis.org/entries');
+    x.onload = function () {
+      if (x.status === 200) {
+        setData(JSON.parse(x.responseText))
+      }
+    }
+    x.send()
+    console.log(data)
+    console.log(data)
+  }
+  
+  // const increase = () =>{
+  //   if(value===10){
+  //     return
+  //   }
+  //   else{
+  //     setCount(value + 1)
+  //   }  }
+  // const deincrease = () =>{
+  //   if(value===0){
+  //     return
+  //   }
+  //   else{
+  //     setCount(value - 1)
+  //   }
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div>{value}</div>
+      <button onClick={increase} > + </button>
+      <button onClick={deincrease} > - </button> */}
     </div>
   );
 }
